f15d5382d41b7b0e3edf9c2534d4d15f3a10982b
James Kaviyil Jose <jamesjose03@gmail.com>
2019-10-29 23:16:30 +0530

    feat: add Navbar

---
+++ website/client/src/components/navbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light e-nav">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse e-nav-item" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Login/Sign Up <span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    </div>
+</template>
+
+<style>
+.e-nav {
+    top: 0;
+    position: fixed;
+    width: 100vw;
+}
+.e-nav-item {
+    margin-left: 82vw;
+}
+</style>
