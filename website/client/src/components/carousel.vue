5274f659acc4b654fd422f17d12a94fd6641507a
James Kaviyil Jose <jamesjose03@gmail.com>
2019-10-30 21:53:26 +0530

    feat: Add Carousel & Backend Empty folder

---
+++ website/client/src/components/carousel.vue
@@ -0,0 +1,35 @@
+<template>
+<div id="e-carousel" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner e-carousel-body">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="../assets/1.jpg" alt="First slide">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Welcome to EventHub!</h5>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="../assets/2.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="../assets/3.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#e-carousel" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#e-carousel" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</template>
+
+<style>
+.e-carousel-body{
+    height: 35vw;
+}
+#e-carousel {
+    margin-top: -0.25vw;
+}
+</style>
--- website/client/src/components/navbar.vue
+++ website/client/src/components/navbar.vue
@@ -1,28 +1,45 @@
 <template>
     <div>
     <nav class="navbar navbar-expand-lg navbar-light bg-light e-nav">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand e-title" href="#">EventHub</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse e-nav-item" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Login/Sign Up <span class="sr-only">(current)</span></a>
+        <button class = "nav-link e-btn">Login </button>
       </li>
     </ul>
+    
   </div>
 </nav>
     </div>
 </template>
 
 <style>
+.e-title{
+  font-family: 'Staatliches', cursive;
+  color: white !important;
+}
 .e-nav {
     top: 0;
     position: fixed;
     width: 100vw;
+    background-color:#660066 !important;
 }
 .e-nav-item {
     margin-left: 82vw;
+}
+.e-btn{
+  border: none;
+  font-family: 'Roboto', sans-serif;
+  background-color: #d896ff !important;
+  border-radius: 40px;
+  width: 128px;
+}
+
+.e-btn:hover{
+  background-color: #efbbff !important;
 }
 </style>
