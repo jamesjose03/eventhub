833f4519f3c84fcf6c8adcadb3b0ce34559795bc
James Kaviyil Jose <jamesjose03@gmail.com>
2019-11-06 22:19:15 +0530

    update: styling of login page done

--- website/client/src/components/LoginArea.vue
+++ website/client/src/components/LoginArea.vue
@@ -1,19 +1,22 @@
 <template>
-    <div>
-        <div class="container">
-        <h1>Login</h1>
+    <div class= "e-login-page">
+        <div class=" card container">
+        <h1 class="e-lhead">Login</h1>
         <p>Welcome back! Login here to register for events or to host them!</p>
         <form>
         <div class="form-group">
-            <input type="email" class="form-control e-login-fields" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            <input type="email" v-model="email" class="e-login-fields" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+         
         </div>
         <div class="form-group">
-            <input type="password" class="form-control e-login-fields" id="exampleInputPassword1" placeholder="Password">
+            <input type="password" class="e-login-fields" id="exampleInputPassword1" placeholder="Password">
         </div>
   
-        <button type="submit" class="btn btn-primary e-btn">Submit</button>
+        <button type="submit" class="btn btn-primary e-btn-1">Login</button>
 </form>
+<br>
+<p>Forgot password? <a href="#">Click Here</a></p>
+<p>Have not yet registered? <a href="">Sign Up</a></p>
     </div>
     </div>
 </template>
@@ -29,5 +32,35 @@
         outline: 0;
         background: transparent;
         border-bottom: 1px solid black;       
+    }
+    .container{
+        background-color: white;
+        margin-top: 25vw;
+        border-radius: 40px;
+        box-shadow: black;
+        height: 25vw;
+        width: 40vw;
+    }
+    .e-login-page{
+        background-color: #d4418e;
+        background-image: linear-gradient(315deg, #d4418e 0%, #0652c5 74%);
+        position: fixed;
+        margin-top: -14vw;
+        height: 100vw;
+        width: 100vw;
+    }
+    .e-lhead{
+        margin-top: 4vw;
+    }
+    .e-btn-1{
+  border: none;
+  font-family: 'Roboto', sans-serif;
+  background-color: #d896ff !important;
+  border-radius: 40px;
+  width: 128px;
+  text-decoration: none;
+    }
+    .e-btn-1:hover{
+        background-color: #efbbff !important;
     }
 </style>
