01ffe82976257f8c07f3160da0cf0fe19a40b148
James Kaviyil Jose <jamesjose03@gmail.com>
2019-11-13 21:07:10 +0530

    feat: add upcoming event section

--- website/client/src/components/DashboardArea.vue
+++ website/client/src/components/DashboardArea.vue
@@ -4,15 +4,15 @@
         <div class="row">
             <div class="col-sm-6">
             <div class="card e-eventcard e-card left-card">
-      <div class="card-body e-body">
+      <div class="card-body e-body" @click="selectedComponent='eupcoming'">
         <i class="fas fa-user-check fa-3x"></i>
         <h5 class="card-title">Upcoming Events</h5>
-        <p class="card-text">Here are your upcoming events:</p>
+        <p class="card-text">Click here for your upcoming events</p>
       </div>
     </div>
   </div>
   <div class="col-sm-6">
-    <div class="card e-eventcard e-card">
+    <div class="card e-eventcard e-card" @click="selectedComponent='eprevious'">
       <div class="card-body e-body">
         <i class="fas fa-calendar-check fa-3x"></i>
         <h5 class="card-title">Previous Events</h5>
@@ -20,11 +20,28 @@
       </div>
     </div>
   </div>
+ 
 </div>
-
+<component :is="selectedComponent"> </component>
 </div>
 </template>
-
+<script>
+import eupcoming from '@/components/Eupcoming.vue'
+import eprevious from '@/components/Eprevious.vue'
+import defaulteventview from '@/components/DefaultEventView.vue'
+export default {
+  data(){
+    return{
+      selectedComponent: 'defaulteventview'
+    }
+  },
+  components: {
+    eupcoming,
+    eprevious,
+    defaulteventview
+  }
+}
+</script>
 <style>
 .dashboard-area{
     margin-top: 1vw;
---
+++ website/client/src/components/Eprevious.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Previous Event  view</h1>
+    </div>
+</template>
