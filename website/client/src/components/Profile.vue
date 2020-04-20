45490410e6e77b030dd769d72f3e84fcd15413ca
James Kaviyil Jose <jamesjose03@gmail.com>
2020-04-20 22:19:16 +0530

    feat: profile page basics

---
+++ website/client/src/components/Profile.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <Navbar />
+        <h1>Welcome, {{ JSON.parse(activeUser) }} !</h1>
+        <i class="far fa-user-circle e-usericon"></i>
+        <i class="fas fa-plus-circle e-addButton"></i>
+        <div class="slider">
+            <a href="#home" class="menu-btn">Profile</a>
+            <a href="#news" class="menu-btn">Events</a>
+            <a href="#contact" class="menu-btn">Certificates</a>
+        </div>
+    </div>
+</template>
+
+<script>
+import Navbar from "@/components/navbar.vue";
+export default {
+    data() {
+        return {
+            activeUser: localStorage.getItem('user')
+        }
+    },
+    components: {
+        Navbar
+    }
+
+}
+</script>
+
+<style>
+.e-usericon {
+    font-size: 100px;
+}
+.e-addButton {
+    font-size: 33px;
+    margin-left: -1vw;
+}
+.menu-btn {
+    margin: 5vw;
+    border: none;
+}
+.slider {
+  background-color: #fff;
+  overflow: hidden;
+}
+.slider a {
+  float: left;
+  display: block;
+  color: #000;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+  border-bottom: 3px solid transparent;
+}
+.slider a:hover {
+  border-bottom: 3px solid purple;
+}
+
+.slider a.active {
+  border-bottom: 3px solid purple;
+}
+</style>
